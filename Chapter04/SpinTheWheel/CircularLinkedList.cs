22190d4ba054619418e45a8271705abb249cd8af
rajdeep-packt <147720422+rajdeep-packt@users.noreply.github.com>
2024-01-25 15:47:40 +0530

    Adding files for code SpinTheWheel

---
+++ Chapter04/SpinTheWheel/CircularLinkedList.cs
@@ -0,0 +1,11 @@
+﻿// SPIN THE WHEEL
+// Chapter 4 (Variants of Lists)
+// C# Data Structures and Algorithms, Second Edition
+
+using System.Collections;
+
+public class CircularLinkedList<T>
+    : LinkedList<T>
+{
+    public new IEnumerator GetEnumerator() => new CircularEnumerator<T>(this);
+}
---
+++ Chapter04/SpinTheWheel/Program.cs
@@ -0,0 +1,49 @@
+﻿// SPIN THE WHEEL
+// Chapter 4 (Variants of Lists)
+// C# Data Structures and Algorithms, Second Edition
+
+CircularLinkedList<string> categories = new();
+categories.AddLast("Sport");
+categories.AddLast("Culture");
+categories.AddLast("History");
+categories.AddLast("Geography");
+categories.AddLast("People");
+categories.AddLast("Technology");
+categories.AddLast("Nature");
+categories.AddLast("Science");
+
+bool isStopped = true;
+Random random = new();
+DateTime targetTime = DateTime.Now;
+int ms = 0;
+foreach (string category in categories)
+{
+    if (isStopped)
+    {
+        Console.WriteLine("Press [Enter] to start.");
+        ConsoleKey key = Console.ReadKey().Key;
+        if (key == ConsoleKey.Enter)
+        {
+            ms = random.Next(1000, 5000);
+            targetTime = DateTime.Now.AddMilliseconds(ms);
+            isStopped = false;
+            Console.WriteLine(category);
+        }
+        else { return; }
+    }
+    else
+    {
+        int remaining = (int)(targetTime - DateTime.Now).TotalMilliseconds;
+        int waiting = Math.Max(100, (ms - remaining) / 5);
+        await Task.Delay(waiting);
+
+        if (DateTime.Now >= targetTime)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            isStopped = true;
+        }
+
+        Console.WriteLine(category);
+        Console.ResetColor();
+    }
+}
