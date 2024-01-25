4cc94940c41b04a119ddeecb4e7a07a8409735e6
rajdeep-packt <147720422+rajdeep-packt@users.noreply.github.com>
2024-01-25 16:19:56 +0530

    Adding code files for Chapter08

---
+++ Chapter08/DirectedWeightedEdges/Program.cs
@@ -0,0 +1,40 @@
+﻿// DIRECTED AND WEIGHTED EDGES
+// Chapter 8 (Exploring Graphs)
+// C# Data Structures and Algorithms, Second Edition
+
+Graph<int> graph = new() { IsDirected = true, IsWeighted = true };
+
+Node<int> n1 = graph.AddNode(1);
+Node<int> n2 = graph.AddNode(2);
+Node<int> n3 = graph.AddNode(3);
+Node<int> n4 = graph.AddNode(4);
+Node<int> n5 = graph.AddNode(5);
+Node<int> n6 = graph.AddNode(6);
+Node<int> n7 = graph.AddNode(7);
+Node<int> n8 = graph.AddNode(8);
+
+graph.AddEdge(n1, n2, 9);
+graph.AddEdge(n1, n3, 5);
+graph.AddEdge(n2, n1, 3);
+graph.AddEdge(n2, n4, 18);
+graph.AddEdge(n3, n4, 12);
+graph.AddEdge(n4, n2, 2);
+graph.AddEdge(n4, n8, 8);
+graph.AddEdge(n5, n4, 9);
+graph.AddEdge(n5, n6, 2);
+graph.AddEdge(n5, n7, 5);
+graph.AddEdge(n5, n8, 3);
+graph.AddEdge(n6, n7, 1);
+graph.AddEdge(n7, n5, 4);
+graph.AddEdge(n7, n8, 6);
+graph.AddEdge(n8, n5, 3);
+
+Console.WriteLine("DFS:");
+graph.DFS().ForEach(Console.WriteLine);
+
+Console.WriteLine("\nBFS:");
+graph.BFS().ForEach(Console.WriteLine);
+
+Console.WriteLine("\nShortest path:");
+List<Edge<int>> path = graph.GetShortestPath(n1, n5);
+path.ForEach(Console.WriteLine);
---
+++ Chapter08/Graph/Node.cs
@@ -0,0 +1,12 @@
+﻿// GRAPH NODE
+// Chapter 8 (Exploring Graphs)
+// C# Data Structures and Algorithms, Second Edition
+
+public class Node<T>
+{
+    public int Index { get; set; }
+    public required T Data { get; set; }
+    public List<Node<T>> Neighbors { get; set; } = [];
+    public List<int> Weights { get; set; } = [];
+    public override string ToString() => $"Index: {Index}. Data: {Data}. Neighbors: { Neighbors.Count}.";
+}
