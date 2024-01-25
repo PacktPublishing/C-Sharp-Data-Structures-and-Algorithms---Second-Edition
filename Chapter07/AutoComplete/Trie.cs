26bd50a5c8b0c90fd85d1dd52ef8c7b16b1c94f0
rajdeep-packt <147720422+rajdeep-packt@users.noreply.github.com>
2024-01-25 16:15:23 +0530

    Adding code files for Chapter07

---
+++ Chapter07/AutoComplete/Trie.cs
@@ -0,0 +1,58 @@
+﻿// AUTO-COMPLETE
+// Chapter 7 (Variants of Trees)
+// C# Data Structures and Algorithms, Second Edition
+
+public class Trie
+{
+    private readonly TrieNode _root = new();
+
+    public bool DoesExist(string word)
+    {
+        TrieNode current = _root;
+        foreach (char c in word)
+        {
+            TrieNode child = current.Children[c - 'a'];
+            if (child == null) { return false; }
+            current = child;
+        }
+        return current.IsWord;
+    }
+
+    public void Insert(string word)
+    {
+        TrieNode current = _root;
+        foreach (char c in word)
+        {
+            int i = c - 'a';
+            current.Children[i] = current.Children[i] ?? new();
+            current = current.Children[i];
+        }
+        current.IsWord = true;
+    }
+
+    public List<string> SearchByPrefix(string prefix)
+    {
+        TrieNode current = _root;
+        foreach (char c in prefix)
+        {
+            TrieNode child = current.Children[c - 'a'];
+            if (child == null) { return []; }
+            current = child;
+        }
+
+        List<string> results = [];
+        GetAllWithPrefix(current, prefix, results);
+        return results;
+    }
+
+    private void GetAllWithPrefix(TrieNode node, string prefix, List<string> results)
+    {
+        if (node == null) { return; }
+        if (node.IsWord) { results.Add(prefix); }
+
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            GetAllWithPrefix(node.Children[c - 'a'], prefix + c, results);
+        }
+    }
+}
---
+++ Chapter07/AutoComplete/TrieNode.cs
@@ -0,0 +1,9 @@
+﻿// AUTO-COMPLETE
+// Chapter 7 (Variants of Trees)
+// C# Data Structures and Algorithms, Second Edition
+
+public class TrieNode
+{
+    public TrieNode[] Children { get; set; } = new TrieNode[26];
+    public bool IsWord { get; set; } = false;
+}
