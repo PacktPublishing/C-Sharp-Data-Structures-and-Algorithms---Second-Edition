23d12ffa6f38518ef2d037486b36c65acad3500e
rajdeep-packt <147720422+rajdeep-packt@users.noreply.github.com>
2024-01-25 16:00:53 +0530

    Adding files for code CallCenterPrioritySupport

---
+++ Chapter05/CallCenterPrioritySupport/CallCenter.cs
@@ -0,0 +1,40 @@
+﻿// CALL CENTER WITH PRIORITY SUPPORT
+// Chapter 5 (Stacks and Queues)
+// C# Data Structures and Algorithms, Second Edition
+
+using Priority_Queue;
+
+public class CallCenter
+{
+    private int _counter = 0;
+    public SimplePriorityQueue<IncomingCall> Calls { get; private set; }
+    public CallCenter() => Calls = new SimplePriorityQueue<IncomingCall>();
+
+    public IncomingCall Call(int clientId, bool isPriority)
+    {
+        IncomingCall call = new()
+        {
+            Id = ++_counter,
+            ClientId = clientId,
+            CallTime = DateTime.Now,
+            IsPriority = isPriority
+        };
+        Calls.Enqueue(call, isPriority ? 0 : 1);
+        return call;
+    }
+
+
+    public IncomingCall? Answer(string consultant)
+    {
+        if (!AreWaitingCalls()) { return null; }
+
+        IncomingCall call = Calls.Dequeue();
+        call.Consultant = consultant;
+        call.AnswerTime = DateTime.Now;
+        return call;
+    }
+
+    public void End(IncomingCall call) => call.EndTime = DateTime.Now;
+
+    public bool AreWaitingCalls() => Calls.Count > 0;
+}
---
+++ Chapter05/CallCenterPrioritySupport/Program.cs
@@ -0,0 +1,27 @@
+﻿// CALL CENTER WITH PRIORITY SUPPORT
+// Chapter 5 (Stacks and Queues)
+// C# Data Structures and Algorithms, Second Edition
+
+Random random = new();
+
+CallCenter center = new();
+center.Call(1234, false);
+center.Call(5678, true);
+center.Call(1468, false);
+center.Call(9641, true);
+
+while (center.AreWaitingCalls())
+{
+    IncomingCall call = center.Answer("Marcin")!;
+    Log($"Call #{call.Id} from client #{call.ClientId} is answered by { call.Consultant}.", call.IsPriority);
+    await Task.Delay(random.Next(1000, 10000));
+    center.End(call);
+    Log($"Call #{call.Id} from client #{call.ClientId} is ended by { call.Consultant}.", call.IsPriority);
+}
+
+void Log(string text, bool isPriority)
+{
+    Console.ForegroundColor = isPriority ? ConsoleColor.Red : ConsoleColor.Gray;
+    Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {text}");
+    Console.ResetColor();
+}
