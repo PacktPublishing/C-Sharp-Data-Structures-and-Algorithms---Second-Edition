dd0a69abe1cb90436a653a695f32e52b7df3f6b7
rajdeep-packt <147720422+rajdeep-packt@users.noreply.github.com>
2024-01-25 16:09:27 +0530

    Adding code files for Chapter06

---
+++ Chapter06/Coupons/Program.cs
@@ -0,0 +1,28 @@
+﻿// COUPONS
+// Chapter 6 (Dictionaries and Sets)
+// C# Data Structures and Algorithms, Second Edition
+
+HashSet<int> usedCoupons = [];
+do
+{
+    Console.Write("Enter the number: ");
+    string number = Console.ReadLine() ?? string.Empty;
+    if (!int.TryParse(number, out int coupon)) { break; }
+
+    if (usedCoupons.Contains(coupon))
+    {
+        Console.WriteLine("Already used.");
+    }
+    else
+    {
+        usedCoupons.Add(coupon);
+        Console.WriteLine("Thank you!");
+    }
+}
+while (true);
+
+Console.WriteLine("\nUsed coupons:");
+foreach (int coupon in usedCoupons)
+{
+    Console.WriteLine(coupon);
+}
