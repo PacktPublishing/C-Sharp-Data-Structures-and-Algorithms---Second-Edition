6c14deeef5f2afc05aff978d3e8e866f1bcc6d2b
rajdeep-packt <147720422+rajdeep-packt@users.noreply.github.com>
2024-05-08 18:00:56 +0530

    Added chapter04 files

---
+++ Chapter04/ArtGallery/Program.cs
@@ -0,0 +1,52 @@
+﻿// ART GALLERY
+// Chapter 4 (Variants of Lists)
+// C# Data Structures and Algorithms, Second Edition
+
+string[][] arts = GetArts();
+CircularLinkedList<string[]> images = new();
+foreach (string[] art in arts) { images.AddLast(art); }
+
+LinkedListNode<string[]> node = images.First!;
+ConsoleKey key = ConsoleKey.Spacebar;
+do
+{
+    if (key == ConsoleKey.RightArrow)
+    {
+        node = node.Next()!;
+    }
+    else if (key == ConsoleKey.LeftArrow)
+    {
+        node = node.Prev()!;
+    }
+
+    Console.Clear();
+    foreach (string line in node.Value)
+    {
+        Console.WriteLine(line);
+    }
+}
+while ((key = Console.ReadKey().Key) != ConsoleKey.Escape);
+
+string[][] GetArts() => [
+    [
+        "  +-----+  ",
+        "o-| o o |-o",
+        "  |  -  |  ",
+        "  +-----+  ",
+        "    | |    "
+    ],
+    [
+        "o +-----+  ",
+        " \\| o o |\\ ",
+        "  |  -  | o",
+        "  +-----+  ",
+        "    / |    "
+    ],
+    [
+        "  +-----+ o",
+        " /| o o |/ ",
+        "o |  -  |  ",
+        "  +-----+  ",
+        "    | \\    "
+    ]
+];
---
+++ Chapter04/AverageValue/Program.cs
@@ -0,0 +1,14 @@
+﻿// AVERAGE VALUE
+// Chapter 4 (Variants of Lists)
+// C# Data Structures and Algorithms, Second Edition
+
+List<double> num = [];
+do
+{
+    Console.Write("Enter the number: ");
+    string numStr = Console.ReadLine() ?? string.Empty;
+    if (!double.TryParse(numStr, out double n)) { break; }
+    num.Add(n);
+    Console.WriteLine($"Average value: {num.Average()}");
+}
+while (true);
