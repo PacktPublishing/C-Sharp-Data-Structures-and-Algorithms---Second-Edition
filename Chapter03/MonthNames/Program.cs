a35bd105e6e71b56317a79e3284b1f0a8ca79500
rajdeep-packt <147720422+rajdeep-packt@users.noreply.github.com>
2024-05-08 17:39:05 +0530

    Adding files for chapter03

---
+++ Chapter03/MonthNames/Program.cs
@@ -0,0 +1,19 @@
+﻿// MONTH NAMES
+// Chapter 3 (Arrays and Sorting)
+// C# Data Structures and Algorithms, Second Edition
+
+using System.Globalization;
+
+CultureInfo culture = new("en");
+string[] months = new string[12];
+for (int month = 1; month <= 12; month++)
+{
+    DateTime firstDay = new(DateTime.Now.Year, month, 1);
+    string name = firstDay.ToString("MMMM", culture);
+    months[month - 1] = name;
+}
+
+foreach (string m in months)
+{
+    Console.WriteLine(m);
+}
