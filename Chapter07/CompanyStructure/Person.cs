a6e3a063b8d3157e1f93724319bf7c4024816138
rajdeep-packt <147720422+rajdeep-packt@users.noreply.github.com>
2024-05-09 10:39:07 +0530

    Delete Chapter07 directory

--- Chapter07/CompanyStructure/Person.cs
+++
@@ -1,5 +0,0 @@
-﻿// COMPANY STRUCTURE
-// Chapter 7 (Variants of Trees)
-// C# Data Structures and Algorithms, Second Edition
-
-public record Person(string Name, string Role);
--- Chapter07/Readme.md
+++
@@ -1,1 +0,0 @@
-Directory for storing chapter07 code files.
