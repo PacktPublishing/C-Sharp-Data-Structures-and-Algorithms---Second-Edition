25f545636e20a83610dfecf805c494d5d7e871dc
rajdeep-packt <147720422+rajdeep-packt@users.noreply.github.com>
2024-05-09 10:51:32 +0530

    Added code files for chapter08

---
+++ 08-Graph/Subset.cs
@@ -0,0 +1,10 @@
+﻿// SUBSET
+// Chapter 8 (Exploring Graphs)
+// C# Data Structures and Algorithms, Second Edition
+
+public class Subset<T>
+{
+    public required Node<T> Parent { get; set; }
+    public int Rank { get; set; }
+    public override string ToString() => $"Rank: {Rank}. Parent: {Parent.Data}. Index: {Parent.Index}.";
+}
---
+++ 08-TelecommunicationCable/Program.cs
@@ -0,0 +1,46 @@
+﻿// TELECOMMUNICATION CABLE
+// Chapter 8 (Exploring Graphs)
+// C# Data Structures and Algorithms, Second Edition
+
+Graph<string> graph = new() { IsDirected = false, IsWeighted = true };
+
+Node<string> nodeB1 = graph.AddNode("B1");
+Node<string> nodeB2 = graph.AddNode("B2");
+Node<string> nodeB3 = graph.AddNode("B3");
+Node<string> nodeB4 = graph.AddNode("B4");
+Node<string> nodeB5 = graph.AddNode("B5");
+Node<string> nodeB6 = graph.AddNode("B6");
+Node<string> nodeR1 = graph.AddNode("R1");
+Node<string> nodeR2 = graph.AddNode("R2");
+Node<string> nodeR3 = graph.AddNode("R3");
+Node<string> nodeR4 = graph.AddNode("R4");
+Node<string> nodeR5 = graph.AddNode("R5");
+Node<string> nodeR6 = graph.AddNode("R6");
+
+graph.AddEdge(nodeB1, nodeB2, 2);
+graph.AddEdge(nodeB1, nodeB3, 20);
+graph.AddEdge(nodeB1, nodeB4, 30);
+graph.AddEdge(nodeB2, nodeB3, 30);
+graph.AddEdge(nodeB2, nodeB4, 20);
+graph.AddEdge(nodeB2, nodeR2, 25);
+graph.AddEdge(nodeB3, nodeB4, 2);
+graph.AddEdge(nodeB4, nodeR4, 25);
+graph.AddEdge(nodeR1, nodeR2, 1);
+graph.AddEdge(nodeR2, nodeR3, 1);
+graph.AddEdge(nodeR3, nodeR4, 1);
+graph.AddEdge(nodeR1, nodeR5, 75);
+graph.AddEdge(nodeR3, nodeR6, 100);
+graph.AddEdge(nodeR5, nodeR6, 3);
+graph.AddEdge(nodeR6, nodeB5, 3);
+graph.AddEdge(nodeR6, nodeB6, 10);
+graph.AddEdge(nodeB5, nodeB6, 6);
+
+Console.WriteLine("Minimum Spanning Tree - Kruskal:");
+List<Edge<string>> kruskal = graph.MSTKruskal();
+kruskal.ForEach(Console.WriteLine);
+Console.WriteLine("Cost: " + kruskal.Sum(e => e.Weight));
+
+Console.WriteLine("\nMinimum Spanning Tree - Prim:");
+List<Edge<string>> prim = graph.MSTPrim();
+prim.ForEach(Console.WriteLine);
+Console.WriteLine("Cost: " + prim.Sum(e => e.Weight));
