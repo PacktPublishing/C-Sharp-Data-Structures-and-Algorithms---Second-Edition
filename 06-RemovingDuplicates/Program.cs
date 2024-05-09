4a2f8157011571ea97bb8ebbbe56da359a89aebe
rajdeep-packt <147720422+rajdeep-packt@users.noreply.github.com>
2024-05-09 10:49:02 +0530

    Added code files for chapter06

---
+++ 06-RemovingDuplicates/Program.cs
@@ -0,0 +1,12 @@
+﻿// REMOVING DUPLICATES
+// Chapter 6 (Dictionaries and Sets)
+// C# Data Structures and Algorithms, Second Edition
+
+List<string> names = [ "Marcin", "Mary", "James", "Albert", "Lily", "Emily", "marcin", "James", "Jane" ];
+SortedSet<string> sorted = new(
+    names,
+    Comparer<string>.Create((a, b) => a.ToLower().CompareTo(b.ToLower())));
+foreach (string name in sorted)
+{
+    Console.WriteLine(name);
+}
---
+++ 06-UserDetails/Employee.cs
@@ -0,0 +1,5 @@
+﻿// USER DETAILS
+// Chapter 6 (Dictionaries and Sets)
+// C# Data Structures and Algorithms, Second Edition
+
+public record Employee(string FirstName, string LastName, string PhoneNumber);
