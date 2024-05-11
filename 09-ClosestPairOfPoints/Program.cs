15762a14df1ddaf3597662a1a949ce3474e37900
Marcin Jamro <124390718+marcinjamro@users.noreply.github.com>
2024-05-11 12:34:59 +0200

    Update Program.cs

--- 09-ClosestPairOfPoints/Program.cs
+++ 09-ClosestPairOfPoints/Program.cs
@@ -1,88 +1,67 @@
-﻿// TITLE GUESS
+// CLOSEST PAIR OF POINTS
 // Chapter 9 (See in Action)
 // C# Data Structures and Algorithms, Second Edition
 
-const string Genes = "abcdefghijklmnopqrstuvwxyz#ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-const string Target = "C# Data Structures and Algorithms";
+List<Point> points =
+[
+    new Point(6, 45),   // A
+    new Point(12, 8),   // B
+    new Point(14, 31),  // C
+    new Point(24, 18),  // D
+    new Point(32, 26),  // E
+    new Point(40, 41),  // F
+    new Point(44, 6),   // G
+    new Point(57, 20),  // H
+    new Point(60, 35),  // I
+    new Point(72, 9),   // J
+    new Point(73, 41),  // K
+    new Point(85, 25),  // L
+    new Point(92, 8),   // M
+    new Point(93, 43)   // N
+];
 
-Random random = new();
-int generationNo = 0;
-List<Individual> population = [];
-
-for (int i = 0; i < 1000; i++)
+points.Sort((a, b) => a.X.CompareTo(b.X));
+Result? closestPair = FindClosestPair(points.ToArray());
+if (closestPair != null)
 {
-    string chromosome = GetRandomChromosome();
-    population.Add(new(chromosome, GetFitness(chromosome)));
+    Console.WriteLine(
+        "Closest pair: ({0}, {1}) and ({2}, {3}) with distance: {4:F2}",
+        closestPair.P1.X,
+        closestPair.P1.Y,
+        closestPair.P2.X,
+        closestPair.P2.Y,
+        closestPair.Distance);
 }
 
-List<Individual> generation = [];
-while (true)
+Result? FindClosestPair(Point[] points)
 {
-    population.Sort((a, b) => b.Fitness.CompareTo(a.Fitness));
-    if (population[0].Fitness == Target.Length)
-    {
-        Print();
-        break;
-    }
+    if (points.Length <= 1) { return null; }
+    if (points.Length <= 3) { return Closest(points); }
 
-    generation.Clear();
+    int m = points.Length / 2;
+    Result r = Closer(
+        FindClosestPair(points.Take(m).ToArray())!,
+        FindClosestPair(points.Skip(m).ToArray())!);
 
-    for (int i = 0; i < 200; i++)
-    {
-        generation.Add(population[i]);
-    }
-
-    for (int i = 0; i < 800; i++)
-    {
-        Individual p1 = population[random.Next(400)];
-        Individual p2 = population[random.Next(400)];
-        Individual offspring = Mate(p1, p2);
-        generation.Add(offspring);
-    }
-
-    population.Clear();
-    population.AddRange(generation);
-    Print();
-    generationNo++;
+    Point[] strip = points.Where(p => Math.Abs(p.X - points[m].X) < r.Distance).ToArray();
+    return Closer(r, Closest(strip));
 }
 
-Individual Mate(Individual p1, Individual p2)
+Result Closest(Point[] points)
 {
-    string child = string.Empty;
-    for (int i = 0; i < Target.Length; i++)
+    Result result = new(points[0], points[0], double.MaxValue);
+    for (int i = 0; i < points.Length; i++)
     {
-        float r = random.Next(101) / 100.0f;
-        if (r < 0.45f) { child += p1.Chromosome[i]; }
-        else if (r < 0.9f) { child += p2.Chromosome[i]; }
-        else { child += GetRandomGene(); }
+        for (int j = i + 1; j < points.Length; j++)
+        {
+            double distance = points[i].GetDistanceTo(points[j]);
+            if (distance < result.Distance) 
+            { 
+                result = new(points[i], points[j], distance); 
+            }
+        }
     }
-
-    return new Individual(child, GetFitness(child));
+    return result;
 }
 
-char GetRandomGene() => Genes[random.Next(Genes.Length)];
-
-string GetRandomChromosome()
-{
-    string chromosome = string.Empty;
-    for (int i = 0; i < Target.Length; i++)
-    { 
-        chromosome += GetRandomGene(); 
-    }
-
-    return chromosome;
-}
-
-int GetFitness(string chromosome)
-{
-    int fitness = 0;
-    for (int i = 0; i < Target.Length; i++)
-    {
-        if (chromosome[i] == Target[i]) { fitness++; }
-    }
-
-    return fitness;
-}
-
-void Print() => Console.WriteLine(
-    $"Generation {generationNo:D2}: {population[0].Chromosome} / {population[0].Fitness}");
+Result Closer(Result r1, Result r2) => r1.Distance < r2.Distance ? r1 : r2;
